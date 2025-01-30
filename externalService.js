689128ae4b0d07676dfd622be43bcc782dfbf31b
ArtemVitr <6996590@stud.nau.edu.ua>
2025-01-30 06:15:02 +0200

    first commit

--- externalService.js
+++
@@ -1,9 +0,0 @@
-function getFactorialFromExternalService(n) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(factorial(n));
-      }, 100);
-    });
-  }
-  
-  module.exports = getFactorialFromExternalService;
--- factorial.js
+++ factorial.js
@@ -1,11 +1,8 @@
-const getFactorialFromExternalService = require('./externalService');
-
-async function factorial(n) {
+function factorial(n) {
   if (n === 0) {
     return 1;
   } else {
-    const result = await getFactorialFromExternalService(n - 1);
-    return n * result;
+    return n * factorial(n - 1);
   }
 }
 
